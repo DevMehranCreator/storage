333a7d0ca2b13d564ad249670a56e4cab349359d
DevMehranCreator <raomin62@gmail.com>
2025-04-08 14:45:12 +0330

    first commmit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+const render = () => {
+  const name = 'mehran';
+  console.log(name);
+  return <span>{name}</span>;
+};
+
+export default function Home() {
+  console.log('test');
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="">fist is not first</div>
+      {render()}
+    </div>
+  );
+}
